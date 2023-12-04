ee24ddbe9751ec25f1cb73ea9a3fe3bfc02273b7
matteberto99 <matteberto99@gmail.com>
2023-12-04 02:10:36 +0100

    c.10

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 //const session = require('express-session');
 //const redis = require('redis');
 //const redisStore = require('connect-redis')(session);
+const multer = require('multer');
 const https = require('https');
 const nodemailer = require('nodemailer');
 const path = require('path');
@@ -15,6 +16,7 @@ const portHTTP = process.env.PORT || "80";
 const portHTTPS = process.env.PORT || "443";
 const options = {key: fs.readFileSync("/etc/letsencrypt/live/italiadelfuturo.it/privkey.pem"), cert:fs.readFileSync("/etc/letsencrypt/live/italiadelfuturo.it/fullchain.pem")};
 const dbconn = mysql.createConnection({host : 'localhost', user : 'root', password : 'Idfdn2023', database : 'idf'});
+const storage = multer.diskStorage({destination: (req, file, cb) => {cb(null, 'articoli/' + parseInt(Date.now()).toString() + '/');},filename: (req, file, cb) => {cb(null, file.originalname);}});
 const mailconn = nodemailer.createTransport({host: "smtps.aruba.it", auth: {user: 'noreply@italiadelfuturo.it', pass: 'Idfdn2023'}, port: 465});
 const domains = ["italiadelfuturo.it"];
 //const redisclient = redis.createClient();
@@ -31,6 +33,8 @@ app.use("/utility", express.static(path.join(__dirname, 'utility')));
 
 const server = https.createServer(options, app);
 dbconn.connect();
+const upload = multer({ storage: storage });
+module.exports = upload;
 
 //---------------------------------------------------------------------------------------------------------------
 
@@ -63,14 +67,14 @@ app.get('/:det', function(req,res)
 	}
 });
 
-app.post('/form_articolo', function(req, res)
+app.post('/form_articolo', upload.single('allegati'), function(req, res)
 {
 	var titolo = req.body.titolo;
-    var allegati = req.files;
-	var timestampInt = parseInt(Date.now());
-	timestampInt = timestampInt.toString();
+    //var allegati = req.files;
+	//var timestampInt = parseInt(Date.now());
+	//timestampInt = timestampInt.toString();
 	
-	var nomeFileArticolo
+	/*var nomeFileArticolo
     for (var file in allegati)
 	{
 		var extension = path.extname(file.filename).toLowerCase();
@@ -80,7 +84,7 @@ app.post('/form_articolo', function(req, res)
 		}
         var filePath = path.join(__dirname, 'articoli/'+timestampInt+'/', file.filename.replace(' ',''));
         fs.rename(file.path, filePath);
-    }
+    }*/
 	
 	//exec('cat *.js bad_file | wc -l');
 	
