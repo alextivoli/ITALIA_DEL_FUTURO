adcd474edb00c7b70091512567ee45a2b2ca73e1
alextivoli <alextivoli@icloud.com>
2023-12-21 15:31:18 +0100

    c19

---
+++ server/js/lang.js
@@ -0,0 +1,25 @@
+var arrLang = {
+    "en-gb": {
+      "HOME": "HOME", 
+      "PARTY": "THE PARTY ",
+      "STATUTE": "THE STATUTE",
+      "CONTACTS": "CONTACTS",
+      "IT":"Italian",
+      "EN":"English",
+      "SDP" : "Find out who we are"
+
+      
+
+    },
+    "it-it": {
+      "HOME": "HOME", 
+      "PARTY": "IL PARTITO",
+      "STATUTE": "LO STATUTO",
+      "CONTACTS": "CONTATTI",
+      "IT":"Italiano",
+      "EN":"Inglese",
+      "SDP" : "Scopri chi siamo"
+      
+      
+    }
+  };
