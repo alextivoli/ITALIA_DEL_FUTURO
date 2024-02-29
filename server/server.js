aa07b66a730006bb99d0a62303f662ab8a8fccd0
Alex Tivoli <alextivoli@icloud.com>
2024-03-01 00:17:57 +0100

    c

--- server/server.js
+++ server/server.js
@@ -16,11 +16,11 @@ const cookieParser = require("cookie-parser");
 const crypto = require('crypto');
 const portHTTP = process.env.PORT || "80";
 const portHTTPS = process.env.PORT || "443";
-const options = {key: fs.readFileSync("/etc/letsencrypt/live/italiadelfuturo.it/privkey.pem"), cert:fs.readFileSync("/etc/letsencrypt/live/italiadelfuturo.it/fullchain.pem")};
-const dbconn = mysql.createConnection({host : 'localhost', user : 'root', password : 'Idfdn2023', database : 'idf'});
+const options = { key: fs.readFileSync("/etc/letsencrypt/live/italiadelfuturo.it/privkey.pem"), cert: fs.readFileSync("/etc/letsencrypt/live/italiadelfuturo.it/fullchain.pem") };
+const dbconn = mysql.createConnection({ host: 'localhost', user: 'root', password: 'Idfdn2023', database: 'idf' });
 //const storage = multer.diskStorage({destination: (req, file, cb) => {const percorso = "./articoli/" + parseInt(Date.now()).toString() + "/"; fs.mkdirSync(percorso,{recursive:true}); cb(null, percorso);},filename: (req, file, cb) => {cb(null, file.originalname);}});
-const storage = multer.diskStorage({destination: (req, file, cb) => {const percorso = "./articoli/"; cb(null, percorso);},filename: (req, file, cb) => {cb(null, file.originalname);}});
-const mailconn = nodemailer.createTransport({host: "smtps.aruba.it", auth: {user: 'noreply@italiadelfuturo.it', pass: 'Idfdn2023'}, port: 465});
+const storage = multer.diskStorage({ destination: (req, file, cb) => { const percorso = "./articoli/"; cb(null, percorso); }, filename: (req, file, cb) => { cb(null, file.originalname); } });
+const mailconn = nodemailer.createTransport({ host: "smtps.aruba.it", auth: { user: 'noreply@italiadelfuturo.it', pass: 'Idfdn2023' }, port: 465 });
 const domains = ["italiadelfuturo.it"];
 //const redisclient = redis.createClient();
 const defaultLang = "it-it"
@@ -28,7 +28,7 @@ const defaultLang = "it-it"
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({secret: "idfKey", saveUninitialized: true, resave: true}));
+app.use(session({ secret: "idfKey", saveUninitialized: true, resave: true }));
 //app.use(session({secret: 'BerTiv2022', store: new redisStore({ host: 'localhost', port: 6379, client: redisclient, ttl: 260}), saveUninitialized: true, resave: false}));
 app.use("/video", express.static(path.join(__dirname, 'video')));
 app.use("/js", express.static(path.join(__dirname, 'js')));
@@ -42,34 +42,31 @@ dbconn.connect();
 const upload = multer({ storage: storage });
 module.exports = upload;
 const transporter = nodemailer.createTransport(
-{
-	host: "smtps.aruba.it",
-	auth:
 	{
-		user: 'noreply@italiadelfuturo.it',
-		pass: 'Idfdn2024!'
-	},
-	port: 465
-});
+		host: "smtps.aruba.it",
+		auth:
+		{
+			user: 'noreply@italiadelfuturo.it',
+			pass: 'Idfdn2024!'
+		},
+		port: 465
+	});
 
 //---------------------------------------------------------------------------------------------------------------
 
 //res.sendFile(indexFile, { root: __dirname + '/public' });
 
-app.use('/', function(req, res, next)
-{
+app.use('/', function (req, res, next) {
 	if (req.url == "/") res.redirect('https://' + req.headers.host + "/home");
 	next();
 });
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/:det', function(req,res)
-{
+app.get('/:det', function (req, res) {
 	var det = req.params.det;
 	var host = req.headers.host;
-	if (host == domains[0])
-    {
+	if (host == domains[0]) {
 		if (det == "home") res.sendFile(__dirname + "/public/index.html");
 		else if (det == "homeprova") res.sendFile(__dirname + "/public/index2.html");
 		else if (det == "news") res.sendFile(__dirname + "/public/news.html");
@@ -83,14 +80,12 @@ app.get('/:det', function(req,res)
 		else if (det == "login") res.sendFile(__dirname + "/public/login.html");
 		else res.status(404).send("Opss! Pagina errata o accesso non consentito.");
 	}
-	else
-	{
+	else {
 		res.status(404).send("Opss! Pagina errata o accesso non consentito.");
 	}
 });
 
-app.post('/form_articolo', upload.array('allegati'), function(req, res)
-{
+app.post('/form_articolo', upload.array('allegati'), function (req, res) {
 	var titolo = req.body.titolo;
 	var orig = './articoli/';
 	var nameFold = parseInt(Date.now()).toString();
@@ -98,50 +93,44 @@ app.post('/form_articolo', upload.array('allegati'), function(req, res)
 	fs.mkdirSync(dest);
 	var rudeList = fs.readdirSync(orig);
 	var files = rudeList.filter(file => fs.statSync(path.join(orig, file)).isFile());
-	files.forEach(file => 
-	{
+	files.forEach(file => {
 		origPath = path.join(orig, file);
 		destPath = path.join(dest, file);
 		fs.renameSync(origPath, destPath);
 	});
-	
-	
+
+
 	rudeList = fs.readdirSync(dest);
-	const fileDoc = rudeList.filter(file =>
-	{
+	const fileDoc = rudeList.filter(file => {
 		var ext = path.extname(file).toLowerCase();
 		return ext === '.doc' || ext === '.docx';
 	});
-	
-	
+
+
 	var command = 'python3 ./utility/wordTohtml.py ' + dest + fileDoc[0] + ' ' + dest + 'page.txt'
 	exec(command);
 	var query = "INSERT INTO articoli(titolo, cartella) VALUES(?, ?)";
 	dbconn.query(query, [titolo, nameFold]); //posso inserire anche l'html dell'articolo nel db
-	
-	
-	dbconn.query('SELECT nome, email FROM newsletter', function(err, rows, fields)
-	{
+
+
+	dbconn.query('SELECT nome, email FROM newsletter', function (err, rows, fields) {
 		var bccList = '';
-		for (var i=0; i<rows.length; i++)
-		{
-			if (i==0) bccList = rows[i].email;
+		for (var i = 0; i < rows.length; i++) {
+			if (i == 0) bccList = rows[i].email;
 			else bccList += ', ' + rows[i].email;
 		}
-		
+
 		console.log(bccList);
-		var mailOptions = 
+		var mailOptions =
 		{
 			from: '"Italia del Futuro" <noreply@italiadelfuturo.it>',
 			bcc: bccList,
 			subject: 'IDF informa',
 			text: 'Ti informiamo del nuovo articolo!',
 		};
-		
-		transporter.sendMail(mailOptions, function(error, info)
-		{
-			if(error)
-			{
+
+		transporter.sendMail(mailOptions, function (error, info) {
+			if (error) {
 				return console.log('Messaggio NON inviato: ' + error);
 			}
 			console.log('Messaggio inviato: ' + info.response);
@@ -150,81 +139,124 @@ app.post('/form_articolo', upload.array('allegati'), function(req, res)
 	});
 });
 
-app.post('/form_mailing', function(req, res)
-{
+app.post('/form_mailing', function (req, res) {
 	var nome = req.body.nome.toUpperCase();
 	var cognome = req.body.surname.toUpperCase();
 	var email = req.body.email.toLowerCase().trim();
 	var query = "INSERT INTO newsletter(nome,cognome,email) VALUES(?,?,?)";
-	dbconn.query(query, [nome, cognome, email], function(err)
-	{
-		if (err) {res.status(200).end("Email già presente!");}
-		else {res.status(200).end("Email aggiunta correttamente!");}
+	dbconn.query(query, [nome, cognome, email], function (err) {
+		if (err) { res.status(200).end("Email già presente!"); }
+		else { res.status(200).end("Email aggiunta correttamente!"); }
 	});
 });
 
-app.post('/requests', function(req,res)
-{
+app.post('/requests', function (req, res) {
 
-	if (req.body.value == "login")
-	{
+	if (req.body.value == "login") {
 		var e = req.body.email;
 		var p = req.body.password;
 		var hash = crypto.createHash('md5').update(p).digest("hex");
-		
-		dbconn.query('SELECT * FROM utenti WHERE username=? AND password=?',[e,hash],function(err, rows, fields)
-        {
-			if (rows.length>0)
-			{
+
+		dbconn.query('SELECT * FROM utenti WHERE username=? AND password=?', [e, hash], function (err, rows, fields) {
+			if (rows.length > 0) {
 				req.session.mail = e;
 				req.session.save();
 				res.send(e);
 			}
-			else
-			{
+			else {
 				res.send('');
 			}
 		});
 	}
-	else if (req.body.value == "check")
-	{
-		if (req.session.mail){res.send(req.session.mail);}else{res.send('');}	
+	else if (req.body.value == "check") {
+		if (req.session.mail) { res.send(req.session.mail); } else { res.send(''); }
 	}
-	else if (req.body.value == "logout")
-	{
+	else if (req.body.value == "logout") {
 		req.session.destroy();
 		res.send('https://italiadelfuturo.it/login/');
 	}
-	else if (req.body.value == "setLang")
-	{
-		if(req.body.lang == "")
-		{
+	else if (req.body.value == "setLang") {
+		if (req.body.lang == "") {
 			req.session.lang = defaultLang;
 		}
-		else
-		{
+		else {
 			req.session.lang = req.body.lang;
 		}
 		req.session.save();
-		res.send(req.session.lang);	
+		res.send(req.session.lang);
 	}
-	else if (req.body.value == "getLang")
-	{
-		if (req.session.lang)
-		{
+	else if (req.body.value == "getLang") {
+		if (req.session.lang) {
 			res.send(req.session.lang);
 		}
-		else
-		{
+		else {
 			res.send('');
 		}
 	}
+	else if (req.body.val == "candidati") {
+		var query = "SELECT c.id , c.nome as nome, c.cognome as cognome  , c.curriculumpdf as curriculumpdf , c.cas_giudizpdf  as cas_giudizpdf  from candidati c ";
+
+		dbconn.query(query, function (err, rows, fields) {
+			if (rows.length > 0) {
+
+				var html = "<tbody>";
+
+				for (var i = 0; i < rows.length; i++) {
+					var htmlRow = "<td>" + rows[i].nome + "</td><td>" + rows[i].cognome + "</td>" + "<td style='text-align: center;'><a href='" + rows[i].curriculumpdf + "' download><i class='fas fa-download'></i></a></td>" + "<td style='text-align: center;'><a href='" + rows[i].cas_giudizpdf + "'download><i class='fas fa-download'></i></a></td>";
+					html += htmlRow;
+				}
+
+			}
+			res.send(html);
+		});
+	}
+	else if (req.body.val == "bilanci") {
+		var query = "SELECT b.id , b.anno as anno  , b.docpdf as docpdf  from bilanci b ";
+
+		dbconn.query(query, function (err, rows, fields) {
+			if (rows.length > 0) {
+
+				var html = "<tbody>";
+
+				for (var i = 0; i < rows.length; i++) {
+					var htmlRow = "<td>" + rows[i].anno + "</td>" + "<td style='text-align: center;'><a href='" + rows[i].docpdf + "' download><i class='fas fa-download'></i></a></td>";
+					html += htmlRow;
+				}
+
+			}
+			res.send(html);
+		});
+	}
+	else if (req.body.val == "raccolte") {
+		var query = "SELECT r.id , r.nome as nome , r.docpdf as docpdf  FROM raccolte r ";
+
+		dbconn.query(query, function (err, rows, fields) {
+			if (rows.length > 0) {
+
+				var html = "<tbody>";
+
+				for (var i = 0; i < rows.length; i++) {
+					var htmlRow = "<td>" + rows[i].nome + "</td>" + "<td style='text-align: center;'><a href='" + rows[i].docpdf + "' download><i class='fas fa-download'></i></a></td>";
+					html += htmlRow;
+				}
+
+			}
+			res.send(html);
+		});
+	}
+	else if (req.body.val == "articoli"){
+
+	}
+	else if (req.body.val == "liste"){
+		
+	}
+
 });
+
 
 //---------------------------------------------------------------------------------------------------------------
 
-http.get('*',function(req, res)
-{ 
+http.get('*', function (req, res) {
 	res.redirect('https://' + req.headers.host + req.url);
 });
 http.listen(portHTTP);
